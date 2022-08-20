c2a6aa20a3665f3fcae617f16941b55e8f0082b3
raghavareddy.m <raghavareddy.m@ivymobility.com>
2022-08-20 18:20:01 +0530

    Spread Operator

---
+++ SpreadOperator..js
@@ -0,0 +1,17 @@
+set1 = [1,2,3,4,5]
+set2 = [...set1,6,7,8,9,0]
+
+console.log(set2);
+set3 = [...set1,...set2]
+console.log(set3);
+
+set1 = {
+    name ="raghu";
+    age = 22;
+
+}
+set2 = {
+    name ="raghu";
+    age = 22;
+
+}
